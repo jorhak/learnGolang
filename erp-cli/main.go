957ad096f67d3728fc3ecf07168f6d9f9db1c19d
Pierluigi Vaca <itfuturovaca@gmail.com>
2026-07-17 17:20:47 +0000

    Sistema ERP CLI

--- erp-cli/main.go
+++ erp-cli/main.go
@@ -6,14 +6,21 @@ func calculateTax(amount float64) float64{
     return amount * 0.13
 }
 
-func main() {
-    company := "ACME ERP"
-    sales := 1500.0
-    tax := calculateTax(sales)
-    total := sales + tax
+func descount(total float64) float64{
+    return total * 0.04
+}
 
+func main() {
+    company := "Tech Corp"
+    sales := 5000.0
+    tax := calculateTax(sales)
+    discount := descount(sales)
+    total := (sales + tax) - discount
+
+    fmt.Println("===== ERP REPORT =====")
     fmt.Println("Empresa:", company)
     fmt.Println("Ventas:", sales)
-    fmt.Println("Impuesto:", tax)
-    fmt.Println("Total:", total)
+    fmt.Println("Impuestos:", tax)
+    fmt.Println("Descuento:", discount) 
+    fmt.Println("Total final:", total)
 }
