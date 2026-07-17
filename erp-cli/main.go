261a74d39e0b030714ebb0725064bdfea604d225
Pierluigi Vaca <itfuturovaca@gmail.com>
2026-07-17 17:03:38 +0000

    ER CLI v1

--- erp-cli/main.go
+++ erp-cli/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func calculateTax(amount float64) float64{
+    return amount * 0.13
+}
+
 func main() {
-    fmt.Println("ERP CLI iniciado")
+    company := "ACME ERP"
+    sales := 1500.0
+    tax := calculateTax(sales)
+    total := sales + tax
+
+    fmt.Println("Empresa:", company)
+    fmt.Println("Ventas:", sales)
+    fmt.Println("Impuesto:", tax)
+    fmt.Println("Total:", total)
 }
