ed58eaea273059467a3d94c20705e961ac7c106e
Pierluigi Vaca <itfuturovaca@gmail.com>
2026-07-21 12:01:19 +0000

    Convertir struct a json sin formato

---
+++ conceptos/archivos-json-persistencia/convertir-struct-json/go.mod
@@ -0,0 +1,3 @@
+module conceptos/archivos-json-persistencia/convertir-struct-json
+
+go 1.26.5
---
+++ conceptos/archivos-json-persistencia/convertir-struct-json/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+import "encoding/json"
+
+type Product struct {
+    ID    int
+    Name  string
+    Price float64
+    Stock int
+}
+
+func main(){
+    product := Product{
+              ID:   1,
+              Name: "Laptop",
+              Price: 1200,
+              Stock: 10,
+    }
+
+    data, err := json.Marshal(product)
+    if err != nil {
+          panic(err)
+    }
+    fmt.Println(string(data))
+}
