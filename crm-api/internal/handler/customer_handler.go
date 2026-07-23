ac8858464e4d9605f7eb57d3d3f75350f50b2bbe
Pierluigi Vaca <itfuturovaca@gmail.com>
2026-07-23 21:26:28 +0000

    Nuevo endpoint Customer y leer Query Parameters

---
+++ crm-api/internal/handler/customer_handler.go
@@ -0,0 +1,28 @@
+package handler
+
+import(
+   "fmt"
+   "net/http"
+)
+
+type CustomerHandler struct {}
+
+func NewCustomerHandler() *CustomerHandler{
+    return &CustomerHandler{}
+}
+
+func (h *CustomerHandler) GetCustomers(
+    w http.ResponseWriter,
+    r *http.Request,
+) {
+    fmt.Println("Metodo:", r.Method)
+    //Leer Url
+    fmt.Println(r.URL.Path,)
+    //Leer Query Parameters
+    page := r.URL.Query().Get("page")
+    limit := r.URL.Query().Get("limit")
+    fmt.Println(page)
+    fmt.Println(limit)
+    w.WriteHeader(http.StatusOK)
+    _, _ = w.Write([]byte("Cambio Listado de Clientes"))
+}
--- crm-api/internal/server/routes.go
+++ crm-api/internal/server/routes.go
@@ -7,6 +7,7 @@ import(
 func (s *Server) routes() {
     healthHandler := handler.NewHealthHandler()
     versionHandler := handler.NewVersionHandler()
+    customerHandler := handler.NewCustomerHandler()
 
     s.mux.HandleFunc(
        "/health",
@@ -16,5 +17,10 @@ func (s *Server) routes() {
     s.mux.HandleFunc(
        "/version",
        versionHandler.Version,
+    )
+
+    s.mux.HandleFunc(
+       "/customers",
+       customerHandler.GetCustomers,
     )
 }
