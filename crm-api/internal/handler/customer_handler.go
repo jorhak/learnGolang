36104f71e1f441bcdaac23502a13600d2c0cb912
Pierluigi Vaca <itfuturovaca@gmail.com>
2026-07-24 00:25:20 +0000

    Se agrego Status, Header,Leer Headers, Leer Query Paramters, Leer Url

--- crm-api/internal/handler/customer_handler.go
+++ crm-api/internal/handler/customer_handler.go
@@ -23,6 +23,20 @@ func (h *CustomerHandler) GetCustomers(
     limit := r.URL.Query().Get("limit")
     fmt.Println(page)
     fmt.Println(limit)
-    w.WriteHeader(http.StatusOK)
+    //Leer Headers
+    token := r.Header.Get("Authorization",)
+    fmt.Println(token)
+    //Enviar Header
+    w.Header().Set(
+       "Content-Type",
+       "application/json",
+    )
+    w.Header().Set(
+      "Author",
+      "SABOR-LATINO",
+    )
+    //Enviar Status
+    w.WriteHeader(http.StatusCreated)
+    //w.WriteHeader(http.StatusOK)
     _, _ = w.Write([]byte("Cambio Listado de Clientes"))
 }
